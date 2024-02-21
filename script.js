4228f28246ba4357dba7d790f16a9d99f092d4f9
Manjot Singh Jolly <manjotsinghjolly@gmail.com>
2024-02-21 18:20:14 +0530

    Create objects for gameboard, players, and gamecontrol

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+// Making the gameboard object
+const Gameboard = {
+  //properties and methods to represent and manipulate the gameboard
+  boardArray: [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ],
+};
 
+// Making the player object
+const playerOne = {
+  //name and marker properties;
+};
+
+const playerTwo = {
+  // name and marker properties;
+};
+
+// Making the game control object
+const gameControl = {
+  // 1. starting the game
+  //2. determining whose turn it is
+  //3. checking for a winner
+  //4. updating the gameboard after each move
+  //5. ending the game when a winner is found or the game ends in a draw
+};
